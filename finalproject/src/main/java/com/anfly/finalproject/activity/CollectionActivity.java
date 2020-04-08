a47692a7a031c415b21beb262cc3849c6b1bae5a
anwanfei <546513254@qq.com>
2020-04-08 15:38:01 +0800

    总复习四

---
+++ finalproject/src/main/java/com/anfly/finalproject/activity/CollectionActivity.java
@@ -0,0 +1,16 @@
+package com.anfly.finalproject.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.anfly.finalproject.R;
+
+public class CollectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collection);
+    }
+}
--- finalproject/src/main/java/com/anfly/finalproject/fragment/HomeFragment.java
+++ finalproject/src/main/java/com/anfly/finalproject/fragment/HomeFragment.java
@@ -1,16 +1,33 @@
 package com.anfly.finalproject.fragment;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.PopupWindow;
 
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.anfly.finalproject.R;
+import com.anfly.finalproject.activity.CollectionActivity;
 import com.anfly.finalproject.adapter.HomeAdapter;
 import com.anfly.finalproject.bean.BannerBean;
 import com.anfly.finalproject.bean.FoodBean;
@@ -32,6 +49,7 @@ public class HomeFragment extends Fragment implements HomeAdapter.OnItemClickLis
     private ArrayList<BannerBean.DataBean> banners;
     private ArrayList<FoodBean.DataBean> list;
     private HomeAdapter adapter;
+    private ConstraintLayout cl_home;
 
 
     public HomeFragment() {
@@ -43,6 +61,7 @@ public class HomeFragment extends Fragment implements HomeAdapter.OnItemClickLis
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View inflate = inflater.inflate(R.layout.fragment_home, container, false);
+        setHasOptionsMenu(true);
         initView(inflate);
         initData();
         return inflate;
@@ -109,6 +128,7 @@ public class HomeFragment extends Fragment implements HomeAdapter.OnItemClickLis
     private void initView(View inflate) {
         rv = inflate.findViewById(R.id.rv);
         srl = inflate.findViewById(R.id.srl);
+        cl_home = inflate.findViewById(R.id.cl_home);
 
         rv.setLayoutManager(new LinearLayoutManager(getActivity()));
 
@@ -121,6 +141,72 @@ public class HomeFragment extends Fragment implements HomeAdapter.OnItemClickLis
 
     @Override
     public void onItemClick(int position) {
-        Toast.makeText(getActivity(), list.get(position).getTitle(), Toast.LENGTH_SHORT).show();
+        showPop();
+    }
+
+    private void showPop() {
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.pop_layout, null);
+
+        Button btn_confirm = view.findViewById(R.id.btn_confirm);
+        Button btn_cancel = view.findViewById(R.id.btn_cancel);
+        final PopupWindow popupWindow = new PopupWindow(view, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        popupWindow.showAtLocation(cl_home, Gravity.CENTER, 0, 0);
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+            }
+        });
+
+        btn_confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+                notification();
+            }
+        });
+    }
+
+    private void notification() {
+        NotificationManager nm = (NotificationManager) getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
+
+        String channelId = "1";
+        String channelName = "final";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT);
+            channel.enableLights(true);
+            channel.setLightColor(Color.RED);
+            nm.createNotificationChannel(channel);
+        }
+
+        Intent intent = new Intent(getActivity(), CollectionActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getActivity(), 200, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification nofication = new NotificationCompat.Builder(getActivity(), channelId)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("最后的项目")
+                .setContentText("可能最后一次了")
+                .setContentIntent(pendingIntent)
+                .build();
+
+        nm.notify(100, nofication);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_home_options, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_collection:
+                Intent intent = new Intent(getActivity(), CollectionActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
